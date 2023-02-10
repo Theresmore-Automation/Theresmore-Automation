6f41e3f21e579cfc041437fb7e3232036b9947c6
Mori <moriakaice@gmail.com>
2023-02-10 08:56:43 +0100

    v2.2.4 - fix bug in dangerous researches

--- dist/release-2.2.4.user.js
+++ dist/release-2.2.4.user.js
@@ -19071,4 +19071,3 @@ const taVersion = "2.2.4";
   init();
 
 })();
-//# sourceMappingURL=bundle.user.js.map
--- src/pages/research.js
+++ src/pages/research.js
@@ -57,10 +57,10 @@ const doResearchWork = async () => {
 
       if (allowedResearch.find((tech) => tech.id === button.innerText.split('\n').shift().trim()).confirm) {
         if (!navigation.checkPage(CONSTANTS.PAGES.RESEARCH, CONSTANTS.SUBPAGES.RESEARCH)) return
+        await sleep(1000)
         const redConfirmButton = [...document.querySelectorAll('.btn.btn-red')].find((button) => button.innerText.includes('Confirm'))
 
         if (redConfirmButton) {
-          await sleep(1000)
           redConfirmButton.click()
           await sleep(4000)
           if (!navigation.checkPage(CONSTANTS.PAGES.RESEARCH, CONSTANTS.SUBPAGES.RESEARCH)) return
